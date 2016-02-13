040aa852343aff9f39bbae907cd94672ca85f7da
root <root@marc-desktop.telus>
2016-02-12 22:37:45 -0800

    Cleaned up code, polished comments

---
+++ child_proc.go
@@ -0,0 +1,275 @@
+/*------------------------------------------------------------------------------
+-- DATE:	       February, 2016
+--
+-- Source File:	 child_proc.go
+--
+-- REVISIONS: 	(Date and Description)
+--
+-- DESIGNER:	   Marc Vouve
+--
+-- PROGRAMMER:	 Marc Vouve
+--
+--
+-- INTERFACE:
+--	func newConnection(listenFd int) (connectionInfo, error)
+--	func hostString(socketAddr syscall.Sockaddr) string
+--  func listen(srvInfo serverInfo)
+--  func addConnectionToEPoll(epFd int, newFd int)
+--  func endConnection(srvInfo serverInfo, conn connectionInfo)
+--  func handleData(conn *connectionInfo) (int, error)
+--  func read(fd int) (string, error)
+--
+--
+--
+-- NOTES: This file is for functions that are part of child go routines which
+--        handle data for the EPoll version of the scalable server.
+------------------------------------------------------------------------------*/
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+const bufferSize = 1024
+
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    newConnection
+--
+-- DATE:        February 6, 2016
+--
+-- REVISIONS:	  February 10, 2016 - modified for EPoll
+--              February 11, 2016 - fixed blocking issues
+--              February 12, 2016 - factored out hostString()
+--
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:   func newConnection(listenFd int) (connectionInfo, error)
+--  listenFd:   the listener file descriptor with the new connection
+--
+-- RETURNS:
+-- connectionInfo: A structor to store data over the life of the connection
+--          error: If an error occurs
+--
+-- NOTES:			this function sets the port into non blocking mode.
+------------------------------------------------------------------------------*/
+func newConnection(listenFd int) (connectionInfo, error) {
+	newFileDescriptor, socketAddr, err := syscall.Accept(int(listenFd))
+	if err != nil {
+		return connectionInfo{}, err
+	}
+	syscall.SetNonblock(newFileDescriptor, true)
+	hostName := hostString(socketAddr)
+
+	return connectionInfo{fileDescriptor: newFileDescriptor, hostName: hostName}, nil
+}
+
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    hostString
+--
+-- DATE:        February 6, 2016
+--
+-- REVISIONS:	  February 12, 2016 - Factored out of new connection
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:   func hostString(socketAddr syscall.Sockaddr) string
+--  socketAddr: the socket address to extract a string of the address from
+--
+-- RETURNS:     the ip as a string or "Unknown" if there is an error
+--
+-- NOTES:			  This function will log if an unknown connection type is found.
+------------------------------------------------------------------------------*/
+func hostString(socketAddr syscall.Sockaddr) string {
+	var hostName string
+	switch socketAddr := socketAddr.(type) {
+	default:
+		log.Printf("%T is not supported, will appear as Uknown in logs", socketAddr)
+		return "Unknown"
+	case *syscall.SockaddrInet4:
+		hostName = net.IPv4(socketAddr.Addr[0], socketAddr.Addr[1], socketAddr.Addr[2], socketAddr.Addr[3]).String()
+		hostName += ":" + strconv.FormatInt(int64(socketAddr.Port), 10)
+	case *syscall.SockaddrInet6:
+		hostName = net.IP(socketAddr.Addr[0:16]).String() + ":" + string(socketAddr.Port)
+	}
+
+	return hostName
+}
+
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    listen
+--
+-- DATE:        February 6, 2016
+--
+-- REVISIONS:	  February 10, 2016 - Modified for EPoll
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:   func listen(srvInfo serverInfo)
+--   srvInfo:   information about the overall application, IPC and networking
+--
+-- RETURNS:     void doesn't actually return.
+--
+-- NOTES:			This function handles Epoll in a foreverloop. This function never
+--            exits
+------------------------------------------------------------------------------*/
+func listen(srvInfo serverInfo) {
+	client := make(map[int]connectionInfo)
+	epollFd, _ := syscall.EpollCreate1(0)
+	events := make([]syscall.EpollEvent, epollQueueLen)
+	addConnectionToEPoll(epollFd, srvInfo.listener)
+	for {
+		_, err := syscall.EpollWait(epollFd, events, -1)
+		if err != nil {
+			continue // need to handle this somehow to prevent a crash. Probably will never be hit
+		}
+		for _, ev := range events {
+			if ev.Fd == int32(srvInfo.listener) { // new connection
+				newClient, err := newConnection(srvInfo.listener)
+				if err == nil {
+					client[newClient.fileDescriptor] = newClient
+					addConnectionToEPoll(epollFd, newClient.fileDescriptor)
+				}
+			} else { // data to read from connection
+				//  client[int(ev.Fd)] can not be used as an argument in handleData
+				conn := client[int(ev.Fd)]
+				err := handleData(&conn)
+				client[int(ev.Fd)] = conn
+				if err != nil {
+					endConnection(srvInfo, client[int(ev.Fd)])
+				}
+			}
+		}
+	}
+}
+
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    addConnectionToEPoll
+--
+-- DATE:        February 10, 2016
+--
+-- REVISIONS:	  February 12, 2016 - Factored out of listen
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:   func addConnectionToEPoll(epFd int, newFd int)
+--      epFd:   the EPoll File Descriptor to add a newFd to.
+--     newFd:   the new file descriptor to add to the epoll file descriptor
+--
+-- RETURNS:     void
+--
+-- NOTES:			this function was created to make listen less verbose. Simply adds
+              newFd to epollFd waiting on the event EPOLLIN
+------------------------------------------------------------------------------*/
+func addConnectionToEPoll(epFd int, newFd int) {
+	event := syscall.EpollEvent{Fd: int32(newFd), Events: syscall.EPOLLIN}
+	syscall.EpollCtl(epFd, syscall.EPOLL_CTL_ADD, newFd, &event)
+}
+
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    endConnection
+--
+-- DATE:        February 10, 2016
+--
+-- REVISIONS:	  February 12, 2016 - Factored out of listen
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:   func addConnectionToEPoll(epFd int, newFd int)
+--   srvInfo:   The server info structure assoiated with the running program
+--      conn:   The connection info for the current host
+--
+-- RETURNS:     void
+--
+-- NOTES:			this function closes the socket and notifies any function listening
+--            on the connectInfo channel.
+------------------------------------------------------------------------------*/
+func endConnection(srvInfo serverInfo, conn connectionInfo) {
+	srvInfo.connectInfo <- conn
+	syscall.Close(conn.fileDescriptor)
+}
+
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    handleData
+--
+-- DATE:        February 7, 2016
+--
+-- REVISIONS:	  February 12, 2016 - made read function its own function.
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:   func handleData(conn *connectionInfo) (int, error)
+--      conn:   structure for the current connection.
+--
+-- RETURNS:     error: io.EOF if the client closed the connection. or system error.
+--              from internal call.
+--
+-- NOTES:			handles an incoming request from a client.
+------------------------------------------------------------------------------*/
+func handleData(conn *connectionInfo) error {
+	msg, err := read(conn.fileDescriptor)
+	if err != nil {
+		return err
+	}
+	syscall.Write(conn.fileDescriptor, []byte(msg))
+	conn.numberOfRequests++
+	conn.ammountOfData += len(msg)
+
+	return nil
+}
+
+/*-----------------------------------------------------------------------------
+-- FUNCTION:    read
+--
+-- DATE:        February 7, 2016
+--
+-- REVISIONS:	  February 12, 2016 - refactored out of handleData function.
+--
+-- DESIGNER:		Marc Vouve
+--
+-- PROGRAMMER:	Marc Vouve
+--
+-- INTERFACE:   func read(fd int) (string, error)
+--      fd:     the file descriptor being read from.
+--
+-- RETURNS:     string: the data read from the client.
+--
+-- NOTES:			reads data from a file descriptor, returns EOF on remote closing
+--            the connection.
+------------------------------------------------------------------------------*/
+func read(fd int) (string, error) {
+	buf := make([]byte, bufferSize)
+	var msg string
+	for {
+		n, err := syscall.Read(fd, buf[:])
+		if err != nil {
+			return "", err
+		}
+		if n == 0 {
+			return "", io.EOF
+		}
+		msg += string(buf[:n])
+		if strings.ContainsRune(msg, '\n') {
+			break
+		}
+	}
+
+	return msg, nil
+}
--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
-// #include <sys/select.h>
-import "C"
-
 import (
 	"container/list"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
@@ -36,118 +32,23 @@ const newConnectionConst = 1
 const finishedConnectionConst = -1
 const epollQueueLen = 1000000
 
-/*******************************************************************************
- * Author Marc Vouve
- *
- * Designer Marc Vouve
- *
- * Date: February 6 2016
- *
- * Params: listenFd: The file descriptor of the listening host
- *
- * Return: connectionInfo of the new connection made
- *
- * Notes: This is a helper function for when a new connection is detected by the
- *        observer loop
- *
- ******************************************************************************/
-func newConnection(listenFd int) (connectionInfo, error) {
-	newFileDescriptor, socketAddr, err := syscall.Accept(int(listenFd))
-	if err != nil {
-		return connectionInfo{}, err
+func main() {
+	if len(os.Args) < 2 { // validate args
+		fmt.Println("Missing args:", os.Args[0], " [PORT]")
+		return
 	}
-	syscall.SetNonblock(newFileDescriptor, true)
-	var hostname string
-	switch socketAddr := socketAddr.(type) {
-	default:
-		return connectionInfo{}, err
-	case *syscall.SockaddrInet4:
-		hostname = net.IPv4(socketAddr.Addr[0], socketAddr.Addr[1], socketAddr.Addr[2], socketAddr.Addr[3]).String()
-		hostname += ":" + strconv.FormatInt(int64(socketAddr.Port), 10)
-	case *syscall.SockaddrInet6:
-		hostname = net.IP(socketAddr.Addr[0:16]).String() + ":" + string(socketAddr.Port)
+	// Create structure to handle server info.
+	srvInfo := newServerInfo()
+	defer srvInfo.Close()
+	// create servers
+	for i := 0; i < 8; i++ {
+		go listen(srvInfo) // spawn listen routines
 	}
-
-	return connectionInfo{fileDescriptor: newFileDescriptor, hostName: hostname}, nil
-}
-
-/* Author: Marc Vouve
- *
- * Designer: Marc Vouve
- *
- * Date: February 6 2016
- *
- * Notes: This function is an "instance" of a server which allows connections in
- *        and echos strings back. After a connection has been closed it will wait
- *        for annother connection
- */
-func serverInstance(srvInfo serverInfo) {
-	client := make(map[int]connectionInfo)
-	epollFd, _ := syscall.EpollCreate1(0)
-	events := make([]syscall.EpollEvent, epollQueueLen)
-	addConnectionToEPoll(epollFd, srvInfo.listener)
-	for {
-		_, err := syscall.EpollWait(epollFd, events, -1)
-		if err != nil {
-			log.Println("err", err)
-			return // block shouldn't be hit under normal conditions. If it does something is really wrong.
-		}
-
-		for _, ev := range events {
-			if ev.Fd == int32(srvInfo.listener) { // new connection
-				newClient, err := newConnection(srvInfo.listener)
-				if err == nil {
-					client[newClient.fileDescriptor] = newClient
-					addConnectionToEPoll(epollFd, newClient.fileDescriptor)
-				}
-			} else { // data to read from connection
-				conn := client[int(ev.Fd)]
-				_, err := handleData(&conn)
-				client[int(ev.Fd)] = conn
-				if err != nil {
-					endConnection(srvInfo, client[int(ev.Fd)])
-				}
-			}
-		}
-	}
-}
-
-func addConnectionToEPoll(epFd int, newFd int) {
-	event := syscall.EpollEvent{Fd: int32(newFd), Events: syscall.EPOLLIN}
-	syscall.EpollCtl(epFd, syscall.EPOLL_CTL_ADD, newFd, &event)
-}
-
-func endConnection(srvInfo serverInfo, conn connectionInfo) {
-	srvInfo.connectInfo <- conn
-	syscall.Close(conn.fileDescriptor)
-}
-
-/**/
-func handleData(conn *connectionInfo) (int, error) {
-	buf := make([]byte, 1024)
-	var msg string
-
-	for {
-		n, err := syscall.Read(conn.fileDescriptor, buf[:])
-		if err != nil {
-			return 0, err
-		}
-		if n == 0 {
-			return len(msg), io.EOF
-		}
-
-		msg += string(buf[:n])
-
-		if strings.ContainsRune(msg, '\n') {
-			break
-		}
-		conn.ammountOfData += n
-
-	}
-	syscall.Write(conn.fileDescriptor, []byte(msg))
-	conn.numberOfRequests++
-
-	return len(msg), nil
+	// when the server is killed it should print statistics need to catch the signal
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, os.Interrupt, os.Kill)
+	// Setup complete
+	manageConnections(srvInfo, osSignals)
 }
 
 /* Author: Marc Vouve
@@ -161,7 +62,7 @@ func handleData(conn *connectionInfo) (int, error) {
  *
  * Notes: This was factored out of the main function.
  */
-func observerLoop(srvInfo serverInfo, osSignals chan os.Signal) {
+func manageConnections(srvInfo serverInfo, osSignals chan os.Signal) {
 	currentConnections := 0
 	connectionsMade := list.New()
 
@@ -182,17 +83,14 @@ func observerLoop(srvInfo serverInfo, osSignals chan os.Signal) {
 }
 
 func newServerInfo() serverInfo {
-	srvInfo := serverInfo{
-		serverConnection: make(chan int, 10), connectInfo: make(chan connectionInfo)}
+	srvInfo := serverInfo{serverConnection: make(chan int, 100), connectInfo: make(chan connectionInfo, 100)}
 	fd, err := syscall.Socket(syscall.AF_INET, syscall.O_NONBLOCK|syscall.SOCK_STREAM, 0)
 	if err != nil {
 		log.Println(err)
 	}
 	syscall.SetNonblock(fd, true)
 	// TODO: make port vairable
-	port, _ := strconv.Atoi(string(os.Args[1]))
-	addr := syscall.SockaddrInet4{Port: port}
-	copy(addr.Addr[:], net.ParseIP("0.0.0.0").To4())
+	addr := getAddr()
 	syscall.Bind(fd, &addr)
 	syscall.Listen(fd, 1000)
 	srvInfo.listener = fd
@@ -200,28 +98,18 @@ func newServerInfo() serverInfo {
 	return srvInfo
 }
 
-func (s serverInfo) Close() {
-	syscall.Close(s.listener)
+func getAddr() syscall.SockaddrInet4 {
+	portStr := strings.SplitAfter(os.Args[1], ":")
+	port, err := strconv.Atoi(portStr[0])
+	if err != nil {
+		log.Fatal("Port must be declaired as :[PORT]")
+	}
+	addr := syscall.SockaddrInet4{Port: port}
+	copy(addr.Addr[:], net.ParseIP("0.0.0.0").To4())
+
+	return addr
 }
 
-func main() {
-	if len(os.Args) < 2 { // validate args
-		fmt.Println("Missing args:", os.Args[0], " [PORT]")
-
-		os.Exit(0)
-	}
-
-	srvInfo := newServerInfo()
-	defer srvInfo.Close()
-
-	// create servers
-	for i := 0; i < 8; i++ {
-		go serverInstance(srvInfo)
-	}
-
-	// when the server is killed it should print statistics need to catch the signal
-	osSignals := make(chan os.Signal, 1)
-	signal.Notify(osSignals, os.Interrupt, os.Kill)
-
-	observerLoop(srvInfo, osSignals)
+func (s serverInfo) Close() {
+	syscall.Close(s.listener)
 }
